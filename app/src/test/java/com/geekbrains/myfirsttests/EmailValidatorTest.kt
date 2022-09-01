00f0c373bae19162e57b18d1debd06164d868d97
Kotik <k-work31@yandex.ru>
2022-09-02 02:56:05 +0300

    lesson1. Добавили несколько тестов в качестве негативного тестирования

--- app/src/test/java/com/geekbrains/myfirsttests/EmailValidatorTest.kt
+++ app/src/test/java/com/geekbrains/myfirsttests/EmailValidatorTest.kt
@@ -40,4 +40,43 @@ class EmailValidatorTest {
     fun emailValidator_NullEmail_ReturnsFalse() {
         assertFalse(EmailValidator.isValidEmail(null))
     }
+
+    @Test
+    fun emailValidator_CyrillicSymbols_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("приветмир@mail.ru"))
+    }
+
+    @Test
+    fun emailValidator_OverflowMaxLengthBeforeEta_ReturnsFalse() {
+        val longEmail = "1234567890123456789012345678901234567890123456789012345678901234567890" +
+                "12345678901234567890123456789012345678901234567890123456789012345678901234567890" +
+                "12345678901234567890123456789012345678901234567890123456789012345678901234567890" +
+                "1234567890123456789012345678901234567890123456789012345678901234567890@yandex.ru"
+
+        assertFalse(EmailValidator.isValidEmail(longEmail))
+    }
+
+    @Test
+    fun emailValidator_OverflowMaxLengthAfterEta_ReturnsFalse() {
+        val longEmail = "dmitrj1992@0000000000111111111122222222223333333333444444444455555555556666666666"
+
+        assertFalse(EmailValidator.isValidEmail(longEmail))
+    }
+
+    @Test
+    fun emailValidator_OverflowMaxLengthAfterDot_ReturnsFalse() {
+        val longEmail = "dmitrj1992@mail.000000000011111111112222222222"
+
+        assertFalse(EmailValidator.isValidEmail(longEmail))
+    }
+
+    @Test
+    fun emailValidator_NotContainsEta_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("приветмир-mail.ru"))
+    }
+
+    @Test
+    fun emailValidator_NotValidSymbols_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("приветмир@mail?.ru"))
+    }
 }
